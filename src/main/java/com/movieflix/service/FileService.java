da1cba57e2bf6570ca106503fc903d5a402c17b9
NazarethV <nazarethvillar38@gmail.com>
2024-08-01 15:39:26 -0300

    feat: add implementation service (FileServiceImpl)

--- src/main/java/com/movieflix/service/FileService.java
+++ src/main/java/com/movieflix/service/FileService.java
@@ -8,10 +8,12 @@ import java.io.InputStream;
 
 
 public interface FileService {
+    //2 MÉTODOS PARA MANIPULAR LOS ARCHIVOS (Cargar el archivo y
 
     //Para cargar el archivo la propiedad necesita la DIRECCIÓN del archivo y el TIPO de archivo
     String uploadFile(String path, MultipartFile file) throws IOException;
 
+    //Necesitamos entregar el archivo, y para entregarlo no podemos tomar el archivo directamente. Por lo que necesitamos convertir el archivo a las secuencias o a los bytes
     InputStream getResourceFile(String path, String name) throws FileNotFoundException;
 
 
