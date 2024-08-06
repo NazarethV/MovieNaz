533c04f6d5a01132eac00eff9f31748a3fd2e5e0
NazarethV <nazarethvillar38@gmail.com>
2024-08-06 13:43:24 -0300

    feat: add controller File

---
+++ src/main/java/com/movieflix/controllers/FileController.java
@@ -0,0 +1,12 @@
+package com.movieflix.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file/")
+public class FileController {
+
+
+}
