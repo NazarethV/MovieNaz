157386455d4c7058db56a9cc329b6f3f86489f0e
NazarethV <nazarethvillar38@gmail.com>
2024-08-06 15:45:56 -0300

    Adding Post File (controller)

--- src/main/java/com/movieflix/controllers/FileController.java
+++ src/main/java/com/movieflix/controllers/FileController.java
@@ -2,8 +2,15 @@ package com.movieflix.controllers;
 
 
 import com.movieflix.service.FileService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/file/")
@@ -15,4 +22,14 @@ public class FileController {
     public FileController(FileService fileService) {
         this.fileService = fileService;
     }
+
+    @Value("${project.poster}") //HACE REFERENCIA A LA RUTA "/POSTER" DEFINIDA EN EL ARCHIVO .YML !!!!!!!!!!
+    private String path;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException {
+        String uploadedFileName = fileService.uploadFile(path, file);
+        return ResponseEntity.ok("File uploaded : " + uploadedFileName);
+    }
+
 }
