004273e5a738cb59673cecfc1b31da588ce9edc0
NazarethV <nazarethvillar38@gmail.com>
2024-08-06 15:34:20 -0300

    adding contructor in the controller

--- src/main/java/com/movieflix/controllers/FileController.java
+++ src/main/java/com/movieflix/controllers/FileController.java
@@ -1,6 +1,7 @@
 package com.movieflix.controllers;
 
 
+import com.movieflix.service.FileService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/file/")
 public class FileController {
 
+    private final FileService fileService;
 
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
 }
