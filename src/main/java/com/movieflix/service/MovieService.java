800c39ef33f593419e62c7fda61a9f2c05ca3395
NazarethV <nazarethvillar38@gmail.com>
2024-08-07 14:07:21 -0300

    Service: MovieService (methods)

--- src/main/java/com/movieflix/service/MovieService.java
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -3,10 +3,13 @@ package com.movieflix.service;
 import com.movieflix.dto.MovieDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface MovieService {
 
     MovieDto addMovie(MovieDto movieDto, MultipartFile file);
 
     MovieDto getMovie(Integer movieId);
 
+    List<MovieDto> getAllMovies();
 }
