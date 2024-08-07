3fd8173a92dc6cfdd046efa831a1d95d33c23d0d
NazarethV <nazarethvillar38@gmail.com>
2024-08-07 13:05:38 -0300

    Adding GetMapping (ServiceFile) in Controller

--- src/main/java/com/movieflix/controllers/FileController.java
+++ src/main/java/com/movieflix/controllers/FileController.java
@@ -2,15 +2,16 @@ package com.movieflix.controllers;
 
 
 import com.movieflix.service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/file/")
@@ -31,5 +32,13 @@ public class FileController {
         String uploadedFileName = fileService.uploadFile(path, file);
         return ResponseEntity.ok("File uploaded : " + uploadedFileName);
     }
+
+    @GetMapping("/{fileName}")
+    public void serveFileHandler(@PathVariable String fileName, HttpServletResponse response) throws IOException{
+        InputStream resourceFile = fileService.getResourceFile(path, fileName);
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        StreamUtils.copy(resourceFile, response.getOutputStream());
+    }
+
 
 }
