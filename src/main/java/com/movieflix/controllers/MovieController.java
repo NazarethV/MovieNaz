d982b1f615b1bb32d8a1fd48a8f68c46fb9dac60
NazarethV <nazarethvillar38@gmail.com>
2024-08-09 15:13:29 -0300

    feat: Controller: addMovie and method convertToMovieDto

--- src/main/java/com/movieflix/controllers/MovieController.java
+++ src/main/java/com/movieflix/controllers/MovieController.java
@@ -1,9 +1,19 @@
 package com.movieflix.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.movieflix.dto.MovieDto;
+import com.movieflix.repositories.MovieRepository;
 import com.movieflix.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/movie")
@@ -19,4 +29,17 @@ public class MovieController {
 
     //Controller q manejará la solicitud para Add/Agregar la Movie/Película a la Base de Datos
     @PostMapping("/add-movie")
+    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file,
+                                                    @RequestPart String movieDto) throws IOException {
+        //Se interactua con la capa de servicio
+        // (Y se convierte Cadena a un Json gracias al método para Convertir)
+        MovieDto dto = convertToMovieDto(movieDto);
+        return new ResponseEntity<>(movieService.addMovie(dto, file), HttpStatus.CREATED);
+    }
+
+    //Creamos un Método Privado para que realice la CONVERSIÓN del Objeto MOVIE q necesitamos
+    private MovieDto convertToMovieDto(String movieDtoObj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(movieDtoObj, MovieDto.class);
+    }
 }
