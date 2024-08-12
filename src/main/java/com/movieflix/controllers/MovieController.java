e6246468031e10131b29a9faadf4e84a1c653dbc
NazarethV <nazarethvillar38@gmail.com>
2024-08-12 13:04:01 -0300

    feat: methods in movieServiceImpl for getMovie and getAllMovies

--- src/main/java/com/movieflix/controllers/MovieController.java
+++ src/main/java/com/movieflix/controllers/MovieController.java
@@ -7,13 +7,11 @@ import com.movieflix.repositories.MovieRepository;
 import com.movieflix.service.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movie")
@@ -37,9 +35,38 @@ public class MovieController {
         return new ResponseEntity<>(movieService.addMovie(dto, file), HttpStatus.CREATED);
     }
 
+
+    //Método para traer UNA SOLA MOVIE (get movie)
+    @GetMapping("/{movieId}")
+    public ResponseEntity<MovieDto> getMovieHandler(@PathVariable Integer movieId) {
+        return ResponseEntity.ok(movieService.getMovie(movieId));
+    }
+
+
+    //Método para traer TODAS las MOVIES
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieDto>> getAllMoviesHandler(){
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
     //Creamos un Método Privado para que realice la CONVERSIÓN del Objeto MOVIE q necesitamos
     private MovieDto convertToMovieDto(String movieDtoObj) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(movieDtoObj, MovieDto.class);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
