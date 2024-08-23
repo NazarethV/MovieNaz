4d618e67d44cc6f2b957aea6a1138682aa5ad366
NazarethV <nazarethvillar38@gmail.com>
2024-08-23 20:34:00 -0300

    Controller: AuthController

---
+++ src/main/java/com/movieflix/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.movieflix.controllers;
+
+import com.movieflix.auth.services.AuthService;
+import com.movieflix.auth.services.JwtService;
+import com.movieflix.auth.services.RefreshTokenService;
+import com.movieflix.auth.utils.AuthResponse;
+import com.movieflix.auth.utils.RegisterRequest;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth/")
+public class AuthController {
+
+    private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtService jwtService;
+
+
+    public AuthController(AuthService authService, RefreshTokenService refreshTokenService, JwtService jwtService) {
+        this.authService = authService;
+        this.refreshTokenService = refreshTokenService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+
+
+
+
+}
