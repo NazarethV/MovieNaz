1610536de75f7ab89fd3cac5266cd1767569bf8b
NazarethV <nazarethvillar38@gmail.com>
2024-08-23 21:28:43 -0300

    ForgotPasswordController: changePasswordHandler - otpGenerator

--- src/main/java/com/movieflix/controllers/ForgotPasswordController.java
+++ src/main/java/com/movieflix/controllers/ForgotPasswordController.java
@@ -4,18 +4,18 @@ import com.movieflix.auth.entities.ForgotPassword;
 import com.movieflix.auth.entities.User;
 import com.movieflix.auth.repositories.ForgotPasswordRepository;
 import com.movieflix.auth.repositories.UserRepository;
+import com.movieflix.auth.utils.ChangePassword;
 import com.movieflix.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.Objects;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/forgotPassword")
@@ -76,10 +76,22 @@ public class ForgotPasswordController {
     }
 
 
+    public ResponseEntity<String> changePasswordHandler(@RequestBody ChangePassword changePassword,
+                                                        @PathVariable String email) {
+        if (!Objects.equals(changePassword.password(), changePassword.repeatPassword())) {
+            return new ResponseEntity<>("Please enter the password again!", HttpStatus.EXPECTATION_FAILED);
+        }
 
+        String encodedPassword = passwordEncoder.encode(changePassword.password());
+        userRepository.updatePassword(email, encodedPassword);
 
+        return ResponseEntity.ok("Password has been changed!");
+    }
 
-
+    private Integer otpGenerator() {
+        Random random = new Random();
+        return random.nextInt(100_000, 999_999);
+    }
 
 
 }
