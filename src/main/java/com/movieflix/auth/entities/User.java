d6c304859a12878f9c85d597725837ccc83fc902
NazarethV <nazarethvillar38@gmail.com>
2024-08-27 12:11:27 -0300

    Details in Entity User

--- src/main/java/com/movieflix/auth/entities/User.java
+++ src/main/java/com/movieflix/auth/entities/User.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -63,7 +64,7 @@ public class User implements UserDetails {
     //Metodos de UserDetails:
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
